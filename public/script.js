e9c7821ca92af120cfa2bb07ccd94ceaf4b2578e
Punit Parmar <punitparmar231@gmail.com>
2025-07-01 20:09:24 +0530

    Update Functionality

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
 document.getElementById('btn').addEventListener('click', () => {
   document.getElementById('result').innerText = "✅ You clicked the button!";
 });
+
+let alertShow = false;
+setInterval(() => {
+  document.title =
+  alertShow ? "DevOps"
+            : "CI/CD Pipeline";
+  
+  alertShow = !alertShow;
+}, 500);
